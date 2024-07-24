4cce514f5ac0a411e65b4cf7c4f614face8e6c9c
rajuljha <rajuljha49@gmail.com>
2024-07-24 17:51:31 +0530

    test new

--- script.sh
+++ script.sh
@@ -1,36 +1,39 @@
 #!/bin/bash
+
 # Prepare docker run command with arguments
 docker_cmd="docker run --rm --name fossologyscanner -w /opt/repo -v ${PWD}:/opt/repo \
-    -e GITHUB_TOKEN=${{ inputs.github_token }} \
-    -e GITHUB_PULL_REQUEST=${{ inputs.github_pull_request }} \
-    -e GITHUB_REPOSITORY=${{ inputs.github_repository }} \
-    -e GITHUB_API=${{ inputs.github_api_url }} \
-    -e GITHUB_REPO_URL=${{ inputs.github_repo_url }} \
-    -e GITHUB_REPO_OWNER=${{ inputs.github_repo_owner }} \
+    -e GITHUB_TOKEN=${GITHUB_TOKEN} \
+    -e GITHUB_PULL_REQUEST=${GITHUB_PULL_REQUEST} \
+    -e GITHUB_REPOSITORY=${GITHUB_REPOSITORY} \
+    -e GITHUB_API=${GITHUB_API_URL} \
+    -e GITHUB_REPO_URL=${GITHUB_REPO_URL} \
+    -e GITHUB_REPO_OWNER=${GITHUB_REPO_OWNER} \
     -e GITHUB_ACTIONS"
 
-if [ "$KEYWORD_CONF_FILE_PATH" != "" ]; then
-docker_cmd+=" -v ${{ github.workspace }}/${{ inputs.keyword_conf_file_path }}:/bin/${{inputskeyword_conf_file_path}}"
+if [ "${KEYWORD_CONF_FILE_PATH}" != "" ]; then
+    docker_cmd+=" -v ${GITHUB_WORKSPACE}/${KEYWORD_CONF_FILE_PATH}:/bin/${KEYWORD_CONF_FILE_PATH}"
 fi
-if [ "${{ inputs.allowlist_file_path }}" != "" ]; then
-docker_cmd+=" -v ${{ github.workspace }}/${{ inputs.allowlist_file_path}}:/bin/${{inputsallowlist_file_path }}"
+if [ "${ALLOWLIST_FILE_PATH}" != "" ]; then
+    docker_cmd+=" -v ${GITHUB_WORKSPACE}/${ALLOWLIST_FILE_PATH}:/bin/${ALLOWLIST_FILE_PATH}"
 fi
 docker_cmd+=" fossology/fossology:scanner /bin/fossologyscanner"
-docker_cmd+=" ${{ inputs.scanners }}"
-docker_cmd+=" ${{ inputs.scan_mode }}"
+docker_cmd+=" ${SCANNERS}"
+docker_cmd+=" ${SCAN_MODE}"
+
 # Add additional conditions
-if [ "${{ inputs.scan_mode }}" == "differential" ]; then
-docker_cmd+=" --tags ${{ inputs.from_tag }} ${{ inputs.to_tag }}"
+if [ "${SCAN_MODE}" == "differential" ]; then
+    docker_cmd+=" --tags ${FROM_TAG} ${TO_TAG}"
 fi
-if [ "${{ inputs.keyword_conf_file_path }}" != "" ]; then
-docker_cmd+=" --keyword-conf ${{ inputs.keyword_conf_file_path }}"
+if [ "${KEYWORD_CONF_FILE_PATH}" != "" ]; then
+    docker_cmd+=" --keyword-conf ${KEYWORD_CONF_FILE_PATH}"
 fi
-if [ "${{ inputs.allowlist_file_path }}" != "" ]; then
-docker_cmd+=" --allowlist-path ${{ inputs.allowlist_file_path }}"
+if [ "${ALLOWLIST_FILE_PATH}" != "" ]; then
+    docker_cmd+=" --allowlist-path ${ALLOWLIST_FILE_PATH}"
 fi
-if [ "${{ inputs.report_format }}" != "" ]; then
-docker_cmd+=" --report ${{ inputs.report_format }}"
+if [ "${REPORT_FORMAT}" != "" ]; then
+    docker_cmd+=" --report ${REPORT_FORMAT}"
 fi
+
 # Run the command
 echo $docker_cmd
 eval $docker_cmd
